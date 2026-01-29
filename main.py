25ced09321ddb52304c3cd41ff4d262573700ee2
Ahad <mohdahad60080@gmail.com>
2026-01-29 17:18:27 +0530

    add path parameter

--- main.py
+++ main.py
@@ -13,3 +13,9 @@ def health_check():
 @app.get("/mission")
 def mission():
     return ({"mission":"Learn Fastapi and build cool shit"})
+
+#path parameter
+@app.get("/items/{item_id}")
+#automatic validation
+def read_item(item_id: int): #type hint, validates it's an integar
+    return {item_id: item_id, "name": f"Item {item_id}"}
