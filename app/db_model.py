aa1a7b01eb9ea4ae5a53bfc2d34f682813a88bf9
Ahad <mohdahad60080@gmail.com>
2026-02-09 14:31:39 +0530

    create SQLAlchemy Model

---
+++ app/db_model.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+
+#Creates a base class, all database models inherit from it.
+Base = declarative_base()
+
+#SQLAlchemy model. It represents the items table.
+class ItemDB(Base):
+    #actual table name in PostgreSQL.
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
+    description = Column(String, nullable=True)
+    cost_price = Column(Float, nullable=False)
+    supplier_secret = Column(String, nullable=False)
+
