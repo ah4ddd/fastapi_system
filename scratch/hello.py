e340ad7e05c400c6a9e192003fe4828b92d8202f
Ahad <mohdahad60080@gmail.com>
2026-04-27 12:30:21 +0530

    hmm ok

--- scratch/hello.py
+++ scratch/hello.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
 
-
 app = FastAPI()
 
-@app.get("/")
-async def root():
+@app.get("/") # path operation decorator
+async def root(): # path operation function
     return {"message": "Hello World"}
+
+"""
+Request
+↓
+Uvicorn (server receives request)
+↓
+ASGI interface
+↓
+Starlette (middleware + routing)
+↓
+FastAPI (validation + dependency)
+↓
+Your function (endpoint)
+↓
+FastAPI (serialize response)
+↓
+Starlette (middleware)
+↓
+Uvicorn (send response)
+↓
+Client
+"""
+
