3aa23ec9aa9de9836e9a82bc1dad6b5bf9012e19
Ahad <mohdahad60080@gmail.com>
2026-04-27 13:13:44 +0530

    path param

--- scratch/hello.py
+++ scratch/hello.py
@@ -6,6 +6,23 @@ app = FastAPI()
 async def root(): # path operation function
     return {"message": "Hello World"}
 
+# Order matters
+# The first one will always be used since the path matches first.
+
+@app.get("users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id):
+    return {"user_id": user_id}
+
+
+@app.get("/items/{item_id}")
+# with the same Python type declaration, FastAPI give you data validation.
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
 """
 Request
 ↓
