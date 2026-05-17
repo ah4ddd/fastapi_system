4a64d4cc6dfb84c6a8e9f4c905e8e39843b0d50d
Ahad <mohdahad60080@gmail.com>
2026-05-17 12:01:01 +0530

    Add key verification function to validate x_key header

--- scratch/hello.py
+++ scratch/hello.py
@@ -213,6 +213,9 @@ async def verify_token(x_token: Annotated[str, Header()]):
     if x_token != "fake-super-secret_token":
         raise HTTPException(status_code=400, detail="X-key header invalid")
 
+async def verify_key(x_key: Annotated[str, Header()]):
+    if x_key != "fake-super-secret-key":
+        return x_key
 
 
 
