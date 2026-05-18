c2ff9c23b2b9473a8fe3bfc1842064dbb047bba9
Ahad <mohdahad60080@gmail.com>
2026-05-18 16:58:29 +0530

    Add POST endpoint for items with item_id parameter

--- scratch/crud.py
+++ scratch/crud.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/hello/")
 async def hello():
     return {"LETS DO IT FROM SCRATCH"}
+
+@app.post("/items/{item_id}/")
+async def items(item_id: str):
+    return f"back at yaa!! : {item_id}"
