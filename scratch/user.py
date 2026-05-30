9d97e624d91a6027db42be1944c0006320425c14
Ahad <mohdahad60080@gmail.com>
2026-05-30 13:30:16 +0530

    Remove unused jwt import from user.py

--- scratch/user.py
+++ scratch/user.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta, timezone
 from typing import Annotated
-
 import jwt
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
