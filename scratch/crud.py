dddc8e7588a6810d4d4f58e47c5a7bd09ff10388
Ahad <mohdahad60080@gmail.com>
2026-06-13 13:25:26 +0530

    Refactor database table creation function in CRUD operations

--- scratch/crud.py
+++ scratch/crud.py
@@ -79,9 +79,9 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
 
-def get_session():
-    with Session(engine) as session:
-        yield session
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
 
 
 @app.post("/addition/")
