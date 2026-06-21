e841159b017270fc56d47b45b2b6326ea0328f51
Ahad <mohdahad60080@gmail.com>
2026-06-21 11:12:44 +0530

    Enhance tests by adding signup functionality

--- app/test_main.py
+++ app/test_main.py
@@ -1,14 +1,49 @@
 from .main import app
 from fastapi.testclient import TestClient
 
+
+"""
+TestClient
+      ↓
+pretends to be browser
+      ↓
+talks directly to FastAPI
+      ↓
+inside memory
+"""
 client = TestClient(app)
 
+"""
+TestClient
+      ↓
+Creates fake HTTP request
+      ↓
+GET /health
+      ↓
+Sends into FastAPI
+"""
 def test_read_health():
     response = client.get("/health")
+    # Expected: 200. Actual: 200. PASS
     assert response.status_code == 200
+    # Expected JSON = Actual JSON. PASS
     assert response.json() == {"message": "System is alive"}
+
 
 def test_background_task():
     response = client.post("/test-background")
     assert response.status_code == 200
     assert response.json() == {"message": "Task scheduled"}
+
+
+def test_signup():
+    response = client.post(
+        "/auth/signup",
+        json={
+            "email": "test@gmail.com",
+            "password": "password123"
+        }
+    )
+
+    assert response.status_code == 201
+
